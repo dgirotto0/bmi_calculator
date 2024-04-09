cd496820d9312c9ee680572668ff1e9419d178a5
dgirotto0 <dgirotto00@gmail.com>
2024-04-09 09:40:33 -0300

    add icon ia

--- lib/result/result_screen.dart
+++ lib/result/result_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../utils/constants.dart';
 import '../widgets/buttons_material.dart';
 import '../widgets/calculate_BMI.dart';
@@ -7,7 +6,7 @@ import '../widgets/cards_info_bmi.dart';
 import '../widgets/description_result_bmi.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +18,12 @@ class ResultScreen extends StatelessWidget {
 }
 
 class ResultBody extends StatelessWidget {
-  const ResultBody({super.key});
+  const ResultBody({Key? key});
 
   @override
   Widget build(BuildContext context) {
     final CalculateBMI calculateBMI =
-        ModalRoute.of(context)!.settings.arguments as CalculateBMI;
+    ModalRoute.of(context)!.settings.arguments as CalculateBMI;
 
     return SafeArea(
       child: Center(
@@ -36,12 +35,31 @@ class ResultBody extends StatelessWidget {
               const Spacer(),
               const CardInfoBMI(),
               const Spacer(),
+              const SizedBox(height: 35), // espaço entre o botão e o ícone
               buildButton(
                 context: context,
                 labelText: 'Try again',
                 onClicked: () => Navigator.of(context).pushNamedAndRemoveUntil(
                   '/homeScreen',
-                  (route) => false,
+                      (route) => false,
+                ),
+              ),
+              const SizedBox(height: 50),
+              FloatingActionButton(
+                backgroundColor: Colors.white,
+                onPressed: () {
+                  // Adicione aqui o que deseja fazer quando o FloatingActionButton for pressionado
+                },
+                child: const CircleAvatar(
+                  radius: 20.0,
+                  backgroundImage: AssetImage('assets/images/natalia.png'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Virtual assistant for more information',
+                style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                  fontSize: 12,
                 ),
               ),
             ],
